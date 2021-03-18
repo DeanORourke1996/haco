1ad7835b1729dac136cd7b0eef6eb7b50920df84
DeanORourke1996 <dean.orourke@hotmail.com>
2021-03-18 20:53:07 +0000

    Adding functions to save request data

--- HacoWeb/haco/globalEventsAPI/data_funcs.py
+++ HacoWeb/haco/globalEventsAPI/data_funcs.py
@@ -21,8 +21,10 @@ def modis_record_fetch():
     url = "https://nrt3.modaps.eosdis.nasa.gov/api/v2/content" \
           "/archives/FIRMS/c6/Global/MODIS_C6_Global_MCD14DL_NRT_"
     date = datetime.datetime.today().strftime('%Y-%m-%d')
+    # Get a Julian date
     today_julian = get_julian_date(date)
     url += str(today_julian)
+    # Append .txt to string as formatted in archive
     url += str(".txt")
     r = requests.get(url, headers=headers)
 
@@ -30,3 +32,18 @@ def modis_record_fetch():
     return r.text
 
 
+# Overwrites data in a local text file
+def write_data():
+    data = modis_record_fetch()
+    file = get_home_dir()
+    file += '/hot_data/VIIRS_DATA_new.txt'
+    with open(file, mode='w') as f:
+        f.write(data)
+
+
+# Process Data Using Pandas
+def data_sense():
+    import pandas as p
+    file = get_home_dir()
+    file += '/hot_data/VIIRS_DATA_new.txt'
+    p.read_csv(file)
